a8f73337a1defe55fb0068cac24ecfeb397b2e2f
Spartalis <peter.spartalis@gmail.com>
2021-11-17 21:07:36 -0800

    Fade Working

--- script.js
+++ script.js
@@ -9,8 +9,8 @@ function blankPad () {
         })
     }
     let n = document.getElementById('size').value
-    if (+n < 16 || +n > 64 || !+n) {
-        alert('You must choose a number between 16 and 64!');
+    if (+n < 16 || +n > 128 || !+n) {
+        alert('You must choose a number between 16 and 128!');
         return;
     } else {
     let pix = 640 / n
@@ -18,16 +18,19 @@ function blankPad () {
     for (let i = 0; i < n**2; i++) {
         let box = document.createElement('div');
         box.classList.add('boxes');
-        box.classList.add('whitebox');
-        box.setAttribute('style', `height: ${pix}px; width: ${pix}px`)
-
+        /*box.classList.add('whitebox');*/
+        box.setAttribute('style', `background-color: rgba(0,0,0,.0); height: ${pix}px; width: ${pix}px`)
         box.addEventListener('mouseover', draw);
         pad.appendChild(box); }
   }
 }
 
 function draw (e) {
-  e.target.classList.add('blackbox');
+  let style = e.target.getAttribute('style');
+  let current = style.charAt(30)
+  let size = style.substring(32)
+  if (+current < 9) {current++}
+  e.target.setAttribute('style', `background-color: rgba(0,0,0,.${current});${size}`);
 }
 
 newbutton.addEventListener('click', function () {blankPad()});
