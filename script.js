b2be9f58992c791d24b420a3684b9db2b4750dd4
Spartalis <peter.spartalis@gmail.com>
2021-11-17 21:34:03 -0800

    Standard and fade working

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 const pad = document.getElementById('pad');
 const newbutton = document.getElementById('createpad');
+let mode
 
 function blankPad () {
     if (document.querySelectorAll('.boxes')) {
@@ -8,6 +9,8 @@ function blankPad () {
             oldBox.remove()
         })
     }
+    mode = document.querySelector('input[type=radio][name=mode]:checked').value;
+    console.log(mode)
     let n = document.getElementById('size').value
     if (+n < 16 || +n > 128 || !+n) {
         alert('You must choose a number between 16 and 128!');
@@ -26,11 +29,17 @@ function blankPad () {
 }
 
 function draw (e) {
+  if (mode == 'standard') {
+    let style = e.target.getAttribute('style');
+    let size = style.substring(32)
+    e.target.setAttribute('style', `background-color: rgba(0,0,0,.9);${size}`)
+
+  } else {
   let style = e.target.getAttribute('style');
   let current = style.charAt(30)
   let size = style.substring(32)
   if (+current < 9) {current++}
-  e.target.setAttribute('style', `background-color: rgba(0,0,0,.${current});${size}`);
+  e.target.setAttribute('style', `background-color: rgba(0,0,0,.${current});${size}`);}
 }
 
 newbutton.addEventListener('click', function () {blankPad()});
